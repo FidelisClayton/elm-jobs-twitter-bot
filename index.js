bafe5e75185352215e967332e69a95bc8faef18a
Clayton Fidelis <clayton.fidelis@pamono.com>
2019-06-19 11:06:28 +0200

    debug: log credentials

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ var client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
+console.log({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+})
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
